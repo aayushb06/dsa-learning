b5c632ffc32336dd4573995b3e178886faa03bd1
Aayush Bhatnagar <abhatnagar@movoto.com>
2024-10-22 13:43:37 +0530

    ll sublist reverse added

--- linkedlist/reverse_ll.py
+++ linkedlist/reverse_ll.py
@@ -46,3 +46,70 @@ Explanation 2:
  In the second example, we want to reverse the highlighted part of the given linked list : 1 -> 4 -> 3 -> 2 -> 5  
  Thus, the output is 5 -> 4 -> 3 -> 2 -> 1 
 """
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def push(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        else:
+            current_node = self.head
+            while current_node.next:
+                current_node = current_node.next
+            current_node.next = new_node
+        
+    def reverse_from_target_to_end(self, start, end):
+        if not self.head or start==end:
+            return None
+        temp = Node(0)
+        temp.next = self.head
+        prev_node = temp
+        i=0
+        while i < start-1:
+           prev_node = prev_node.next
+           i+=1
+        
+        current_node = prev_node.next
+        next_node = None
+        previous_sublist = prev_node
+        remaining_sublist = current_node
+        
+        for _ in range(end-start+1):
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        
+        
+        previous_sublist.next.next = current_node
+        previous_sublist.next = prev_node
+        self.head = temp.next
+    
+    def print_ll(self):
+        current_node = self.head
+        result = ''
+        while current_node:
+            result = result + " "+ str(current_node.value) if result else str(current_node.value)
+            current_node = current_node.next
+        print(result)
+        
+if __name__ == '__main__':
+    linkedlist = LinkedList()
+    linkedlist.push(2)
+    linkedlist.push(3)
+    linkedlist.push(4)
+    linkedlist.push(5)
+    linkedlist.push(6)
+    linkedlist.push(7)
+    linkedlist.print_ll()
+    linkedlist.reverse_from_target_to_end(start=2, end=4)
+    linkedlist.print_ll()
