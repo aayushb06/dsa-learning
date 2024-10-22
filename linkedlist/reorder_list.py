2a36dabd481fa305ede1d73c19642b484602a366
Aayush Bhatnagar <abhatnagar@movoto.com>
2024-10-22 16:16:47 +0530

    reorder ll

--- linkedlist/reorder_list.py
+++ linkedlist/reorder_list.py
@@ -50,3 +50,78 @@ Explanation 2:
 
  The array will be arranged to [A0, An, A1, An-1, A2].
 """
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+def reverse(head):
+    if not head:
+        return None
+    current = head
+    prev = None
+    while current:
+        next_node = current.next
+        current.next = prev
+        prev = current
+        current = next_node
+    head = prev
+    return head
+    
+  
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def push(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            return
+        else:
+            current_node = self.head
+            while current_node.next:
+                current_node = current_node.next
+            current_node.next = new_node
+    
+    def reorder_ll(self):
+        if not self.head:
+            return None
+        slow_ptr = self.head
+        fast_ptr = self.head
+        while fast_ptr and fast_ptr.next:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+        second_half_ll = slow_ptr
+        reversed_second_half = reverse(second_half_ll)
+        
+        first, second = self.head, reversed_second_half
+        while second.next:
+            temp1 = first.next
+            temp2 = second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
+    
+    def print_ll(self):
+        current_node = self.head
+        result = ''
+        while current_node:
+            result = result + " "+ str(current_node.value) if result else str(current_node.value)
+            current_node = current_node.next
+        print(result)
+        
+if __name__ == '__main__':
+    linkedlist = LinkedList()
+    linkedlist.push(2)
+    linkedlist.push(3)
+    linkedlist.push(4)
+    linkedlist.push(5)
+    linkedlist.push(6)
+    linkedlist.print_ll()
+    linkedlist.reorder_ll()
+    linkedlist.print_ll()
+
