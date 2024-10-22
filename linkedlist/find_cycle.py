91e1a953ad361dff702691f0ec64bec017d719a6
Aayush Bhatnagar <abhatnagar@movoto.com>
2024-10-22 15:09:17 +0530

    added code for detect cycle in ll

--- linkedlist/find_cycle.py
+++ linkedlist/find_cycle.py
@@ -28,3 +28,44 @@ Line 1 ( Corresponds to arg 1 ) : Elements in the linked list. First number S is
 Line 2 : Integer X corresponding to a node position. If the integer is -1, then there is no loop. Otherwise, the end node has a next edge to node number X.
 	For example, Integer: "-1" will be written as "-1"(without quotes).
 """
+
+# Floyd's Cycle Detection Algorithm (also known as the Tortoise and Hare algorithm)
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def find_cycle(head):
+    if not head and head.next:
+        return None
+    slow_ptr = head
+    fast_ptr = head
+    while fast_ptr and fast_ptr.next:
+        slow_ptr = slow_ptr.next
+        fast_ptr = fast_ptr.next.next
+        if slow_ptr == fast_ptr:
+            break
+    else:
+        return -1
+    slow_ptr = head
+    while slow_ptr != fast_ptr:
+        slow_ptr = slow_ptr.next
+        fast_ptr = fast_ptr.next
+    return slow_ptr.value
+
+
+if __name__ == '__main__':
+    head = Node(1)
+    second = Node(2)
+    third = Node(3)
+    forth = Node(4)
+    fifth = Node(5)
+    sixth = Node(6)
+    head.next = second
+    second.next = third
+    third.next = forth
+    forth.next = fifth
+    fifth.next = sixth
+    sixth.next = third
+    print(find_cycle(head))
